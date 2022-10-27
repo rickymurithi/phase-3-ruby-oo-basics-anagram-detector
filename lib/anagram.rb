24701ecebf60aa5334037102d5e6762264778a4f
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-27 10:06:04 +0300

    add files

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match (words)
+        words.filter do |word|
+            @word.chars.sort == word.chars.sort
+        end
+    end
+end
